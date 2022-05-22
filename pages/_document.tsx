273f8258cfb7aece52f60ea24948e9cf65e64fe3
nike1v <dgygurdenok@gmail.com>
2022-05-22 23:59:47 +0300

    add fb widget

---
+++ pages/_document.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @next/next/inline-script-id */
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* code that you want to add to the header */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          {/* <!-- Messenger Chat Plugin Code --> */}
+          <div id="fb-root"></div>
+
+          {/* <!-- Your Chat Plugin code --> */}
+          <div id="fb-customer-chat" className="fb-customerchat">
+          </div>
+
+          <Script dangerouslySetInnerHTML={{
+            __html: `
+				var chatbox = document.getElementById('fb-customer-chat');
+				chatbox.setAttribute("page_id", "104657677549573");
+				chatbox.setAttribute("attribution", "biz_inbox");	
+				`
+          }} />
+
+          {/* <!-- Your SDK code --> */}
+          <Script dangerouslySetInnerHTML={{
+            __html: `
+			window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v13.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+			`
+          }} />
+        </body>
+      </Html>
+    );
+  }
+}
