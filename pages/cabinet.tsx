382c70635b7d4e5ad86a9696f961b3f139d3071f
nike1v <dgygurdenok@gmail.com>
2022-05-22 04:32:20 +0300

    update to client render

--- pages/cabinet.tsx
+++ pages/cabinet.tsx
@@ -1,24 +1,19 @@
 import type { NextApiRequest, NextPage } from "next";
 import useTranslation from "next-translate/useTranslation";
-import { signOut, useSession } from "next-auth/react";
+import { getSession, signOut, useSession } from "next-auth/react";
 import Context from "../components";
 import styles from "../styles/Cabinet.module.css";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { PrismaClient } from "@prisma/client";
-import { getToken, JWT } from "next-auth/jwt";
 import { OrderClientType, OrdersType, UserClientType } from "../types/types";
 import Image from "next/image";
 import Link from "next/link";
 import OrdersTable from "../components/OrdersTable";
-import axios from "axios";
 
-export async function getServerSideProps({ req }: { req: NextApiRequest }) {
+export async function getStaticProps() {
   const prisma = new PrismaClient();
-  const token: null | JWT = await getToken({
-    req,
-    secret: process.env.NEXT_JWT_SECRET,
-  });
+  const token = await getSession();
   const user = await prisma.clients.findFirst({
     where: {
       email: token?.email as string | undefined,
@@ -193,7 +188,7 @@ const Home: NextPage<Props> = ({ user, orders }) => {
             <span>
               {t("yourOrders")}
             </span>}
-            <OrdersTable orders={ordersListFiltered} user={user} orderUpdate={handleOrdersFilter} userNames={userNames} services={services} staffNames={staffNames} />
+            {orders && <OrdersTable orders={ordersListFiltered} user={user} orderUpdate={handleOrdersFilter} userNames={userNames} services={services} staffNames={staffNames} />}
           </div>    
         </div>
       </main>
